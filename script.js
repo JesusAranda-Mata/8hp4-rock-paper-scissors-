bd843cf75af0d25f56ae37825597e7a53c6cf1f1
Jesus <jesus.aranda7@yahoo.com>
2020-06-05 13:50:16 -0500

    able to make choices and display the results of the match, front end cosmetics needed

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ function rpsGame(yourChoice) {
     console.log (yourChoice);
     var humanChoice, botChoice;
     humanChoice = yourChoice.id;
+    
     botChoice = numberToChoice(randToRpsInt());
     console.log('computer Choice:', botChoice);
+    
     results = decideWinner(humanChoice, botChoice); //[0,1] human lost | bot won.
     console.log(results);
-    //massage = finalMassage(results);// {'massage' : 'you won!', 'color': 'green'}
-    //rpsFrontEnd(yourChoice.id, botChoice, massage);
+    
+    massage = finalMassage(results);// {'massage' : 'you won!', 'color': 'green'}
+    console.log(massage);
+    
+    rpsFrontEnd(yourChoice.id, botChoice, massage);
 }
 
 function randToRpsInt() {
@@ -32,3 +37,45 @@ function decideWinner (yourChoice, computerChoice) {
     
     return [yourScore, computerChoice];
 }
+
+function finalMassage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+        return {'massage': 'You lost!', 'color': 'red'};        
+    } else if (yourScore === 0.5) {
+        return {'massage': 'You Tied!', 'color': 'yellow'};
+    } else {
+        return {'massage': 'You won!', 'color': 'green'};
+    }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMassage) {
+    var imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src 
+    }
+//remove all images.    
+    document.getElementById('rock').remove(); 
+    document.getElementById('paper').remove(); 
+    document.getElementById('scissors').remove(); 
+
+  var humanDiv = document.createElement('div');
+  var botDiv = document.createElement('div');
+  var massageDiv = document.createElement('div');
+
+  humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + " ' height=150 width=150 style= 'box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+  botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + " ' height=150 width=150 style= 'box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+      
+  document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+  document.getElementById('flex-box-rps-div').appendChild(botDiv);
+}
+
+
+
+
+
+
+
+
+
+
